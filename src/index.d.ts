fc1bc183af81a009517b7dd17ef30e0474d41246
Aloinn <46081985+Aloinn@users.noreply.github.com>
2022-05-02 18:33:59 -0400

    fixed typing

---
+++ src/index.d.ts
@@ -0,0 +1,1 @@
+export * as interfaces from "./main/interfaces";
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
-import * as functions from "./main/function";
-import * as interfaces from "./main/interfaces";
-
-export default { ...functions, ...interfaces };
+export * as functions from "./main/function";
